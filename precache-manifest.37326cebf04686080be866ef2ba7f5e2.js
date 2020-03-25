150d8ac9be56e349cc9370a9e236b1bcd038d679
divesh9205 <divesh9205@gmail.com>
2020-03-25 16:11:48 +0530

    Updates

---
+++ precache-manifest.37326cebf04686080be866ef2ba7f5e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d33dad0a444bb158b5b9f8c0571e46",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "1c09b1b8b4e864fea195",
+    "url": "/monsters-app/static/css/main.3ff166e8.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-app/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-app/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c09b1b8b4e864fea195",
+    "url": "/monsters-app/static/js/main.e988bdab.chunk.js"
+  },
+  {
+    "revision": "254fc56a4548c1840f48",
+    "url": "/monsters-app/static/js/runtime-main.dff89ef1.js"
+  }
+]);
